e1ca658dde646a07a06e4886c36ddca45ffdf06f
Dzmitry Safin <dzmitry.safin@gmail.com>
2017-09-12 21:20:59 +0300

    First sample

---
+++ OpenglSuperBible/Chapter5Sample1.cpp
@@ -0,0 +1,9 @@
+//
+//  Chapter5Sample1.cpp
+//  OpenglSuperBible
+//
+//  Created by Dzmitry Safin on 9/12/17.
+//  Copyright © 2017 Dzmitry Safin. All rights reserved.
+//
+
+#include "Chapter5Sample1.hpp"
---
+++ OpenglSuperBible/Chapter5Sample1.hpp
@@ -0,0 +1,14 @@
+//
+//  Chapter5Sample1.hpp
+//  OpenglSuperBible
+//
+//  Created by Dzmitry Safin on 9/12/17.
+//  Copyright © 2017 Dzmitry Safin. All rights reserved.
+//
+
+#ifndef Chapter5Sample1_hpp
+#define Chapter5Sample1_hpp
+
+#include <stdio.h>
+
+#endif /* Chapter5Sample1_hpp */
